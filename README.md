c79d2603dada55c67e96ab78d1a9d09acda305ab
iamprincesly <iamprincesly@gmail.com>
2021-11-25 00:50:16 +0100

    Releasing the initial version (0.1.0)

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # Erroran
-#### Error handler for node js app
+Erroran is a node.js package for providing error handling middleware in express app.
+
+**[Follow me (@iamprincesly) on Twitter!](https://twitter.com/iamprincesly)**
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Options](#options)
+* [License](#license)
+* [Author](#author)
+
+## Installation
+
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/). Installation is done using the
+[`npm install` command](https://docs.npmjs.com/getting-started/installing-npm-packages-locally):
+
+```sh
+$ npm install erroran
+```
+
+## Usage
+
+```javascript
+var express = require('express');
+var Erroran = require('erroran');
+var app = express();
+
+// Make sure this is always your last middleware
+app.use(Erroran.handler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+```
+
+### Using Erroran to send operational error to client
+
+```javascript
+var express = require('express');
+var Erroran = require('erroran');
+var app = express();
+
+app.get('/products/:id', cors(), function (req, res, next) {
+    return next(
+        Erroran.notAuthenticated('You are not logged in! Please log in to get access');
+    );
+})
+
+// Make sure this is always your last middleware
+app.use(Erroran.handler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+```
+
+## Options
+### Send 404 Not Found Error
+This will automatically send 404 status code and your desire message to the client
+```javascript
+var Erroran = require('erroran');
+
+Erroran.notFound('Resrouce not found');
+```
+
+### Send 400 Bad Request Error
+This will automatically send 400 status code and your desire message to the client
+```javascript
+var Erroran = require('erroran');
+
+Erroran.badRequest('Please provide a valid email');
+```
+
+### Send 401 Not Authenticated Error
+This will automatically send 401 status code and your desire message to the client
+```javascript
+var Erroran = require('erroran');
+
+Erroran.notAuthenticated('Please login to access this resource');
+```
+
+### Send 403 Forbidden Error
+This will automatically send 403 status code and your desire message to the client
+```javascript
+var Erroran = require('erroran');
+
+Erroran.forbidden('You are not permitted to access this resource');
+```
+
+### Send 500 Internal Server Error
+This will automatically send 500 status code and your desire message to the client
+```javascript
+var Erroran = require('erroran');
+
+Erroran.internalServer('Oh no, something went wrong');
+```
+
+## License
+
+[MIT License](http://www.opensource.org/licenses/mit-license.php)
+
+## Author
+
+[Sylvanus Etim](https://github.com/iamprincesly) ([iamprincesly@gmail.com](mailto:iamprincesly@gmail.com))
---
+++ libs/erroran.js
@@ -0,0 +1,167 @@
+'use strict';
+
+/**
+ * Erroran
+ * @author Sylvanus Etim <iamprincesly@gmail.com>
+ * @copyright Erroran Copyright(c) 2021
+ * @license MIT Licensed
+ */
+const BaseError = require('./base');
+
+/**
+ * Do not instantial this class directly
+ * just call the static methods on the class
+ * @class Erroran
+ */
+class Erroran {
+    static OK = 200;
+    static BAD_REQUEST = 400;
+    static NOT_FOUND = 404;
+    static NOT_AUTHENTICATED = 401;
+    static FORBIDDEN = 403;
+    static INTERNAL_SERVER = 500;
+
+    /**
+     * This will automatically send 404 status code 
+     * and your desire message to the client
+     * @static
+     * @param {*} message
+     * @param {*} [statusCode=this.NOT_FOUND]
+     * @param {string} [description='Not Found.']
+     * @param {boolean} [isOperational=true]
+     * @return {*} 
+     * @memberof Erroran
+     */
+    static notFound(
+        message,
+        statusCode = this.NOT_FOUND,
+        description = 'Not Found.',
+        isOperational = true
+    ) {
+        if (!message) throw new Error('Please enter error message');
+
+        return new BaseError(message, statusCode, description, isOperational);
+    }
+
+    /**
+     * This will automatically send 400 status code 
+     * and your desire message to the client
+     * @static
+     * @param {*} message
+     * @param {*} [statusCode=this.BAD_REQUEST]
+     * @param {string} [description='Bad Request.']
+     * @param {boolean} [isOperational=true]
+     * @return {*} 
+     * @memberof Erroran
+     */
+    static badRequest(
+        message,
+        statusCode = this.BAD_REQUEST,
+        description = 'Bad Request.',
+        isOperational = true
+    ) {
+        if (!message) throw new Error('Please enter error message');
+
+        return new BaseError(message, statusCode, description, isOperational);
+    }
+
+    /**
+     * This will automatically send 401 status code 
+     * and your desire message to the client
+     * @static
+     * @param {*} message
+     * @param {*} [statusCode=this.NOT_AUTHENTICATED]
+     * @param {string} [description='Not Authorized.']
+     * @param {boolean} [isOperational=true]
+     * @return {*} 
+     * @memberof Erroran
+     */
+    static notAuthenticated(
+        message,
+        statusCode = this.NOT_AUTHENTICATED,
+        description = 'Not Authorized.',
+        isOperational = true
+    ) {
+        if (!message) throw new Error('Please enter error message');
+
+        return new BaseError(message, statusCode, description, isOperational);
+    }
+
+    /**
+     * This will automatically send 403 status code 
+     * and your desire message to the client
+     *
+     * @static
+     * @param {*} message
+     * @param {*} statusCode
+     * @param {*} [description=this.FORBIDDEN]
+     * @param {string} [description='Forbidden.']
+     * @param {boolean} [isOperational=true]
+     * @return {*} 
+     * @memberof Erroran
+     */
+    static forbidden(
+        message,
+        statusCode,
+        description = this.FORBIDDEN,
+        description = 'Forbidden.',
+        isOperational = true
+    ) {
+        if (!message) throw new Error('Please enter error message');
+
+        return new BaseError(message, statusCode, description, isOperational);
+    }
+
+    /**
+     * This will automatically send 500 status code 
+     * and your desire message to the client
+     *
+     * @static
+     * @param {*} message
+     * @param {*} [statusCode=this.INTERNAL_SERVER]
+     * @param {string} [description='Internal Server Error.']
+     * @param {boolean} [isOperational=true]
+     * @return {*} 
+     * @memberof Erroran
+     */
+    static internalServer(
+        message,
+        statusCode = this.INTERNAL_SERVER,
+        description = 'Internal Server Error.',
+        isOperational = true
+    ) {
+        if (!message) throw new Error('Please enter error message');
+
+        return new BaseError(message, statusCode, description, isOperational);
+    }
+
+    /**
+     * Call this middleware as your last middleware 
+     * in your server script.
+     *
+     * @static
+     * @param {*} err
+     * @param {*} req
+     * @param {*} res
+     * @param {*} next
+     * @memberof Erroran
+     */
+    static handler = (err, req, res, next) => {
+        if (process.env.NODE_ENV === 'production') {
+            return res.status(err.statusCode).json({
+                status: err.status,
+                description: err.description,
+                message: err.message,
+            });
+        }
+
+        return res.status(err.statusCode).json({
+            status: err.status,
+            description: err.description,
+            message: err.message,
+            stack: err.stack,
+        });
+    };
+}
+
+module.exports = Erroran;
